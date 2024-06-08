9248d0ce46b61e5011826f000cae830a0936ab17
Kapil Nagarkoti <kapilnagarkoti0@gmail.com>
2024-06-08 17:54:53 +0545

    made responsive

--- background-changer/src/App.jsx
+++ background-changer/src/App.jsx
@@ -7,7 +7,7 @@ const[color, setColor] = useState('#796EA8');
 
   return (
     <div className='w-full h-lvh flex justify-center items-end' style={{backgroundColor: color, color:'white'}}>
-      <div id="colorTab" className='bg-white w-3/4 h-auto mb-5 rounded-2xl p-3 flex flex-wrap justify-between'>
+      <div id="colorTab" className='bg-white w-3/4 h-auto mb-5 rounded-2xl p-3 flex flex-wrap justify-between gap-3'>
         <div className="colorBtn bg-red-500 px-3 py-1 rounded-2xl transform transition duration-300 ease-in-out hover:scale-110" onClick={() => setColor('red')}>
           <button >Red</button>
         </div>
